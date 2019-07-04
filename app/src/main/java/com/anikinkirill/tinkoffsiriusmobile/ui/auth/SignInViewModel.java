131e37f9ac552eb79981c6c1b81611777a66b778
ANIKINKIRILL <ky.anikin@mail.ru>
2019-07-04 08:52:51 +0300

    SignInViewModel is working

--- app/src/main/java/com/anikinkirill/tinkoffsiriusmobile/ui/auth/SignInViewModel.java
+++ app/src/main/java/com/anikinkirill/tinkoffsiriusmobile/ui/auth/SignInViewModel.java
@@ -1,5 +1,7 @@
 package com.anikinkirill.tinkoffsiriusmobile.ui.auth;
 
+import android.util.Log;
+
 import androidx.lifecycle.ViewModel;
 
 import javax.inject.Inject;
@@ -10,9 +12,11 @@ import javax.inject.Inject;
 
 public class SignInViewModel extends ViewModel {
 
+    private static final String TAG = "SignInViewModel";
+
     @Inject
     public SignInViewModel(){
-
+        Log.d(TAG, "SignInViewModel: viewmodel is working...");
     }
 
 }
