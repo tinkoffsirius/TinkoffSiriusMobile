3f69a705b0e1669113205df91186fb35b11f9e38
ANIKINKIRILL <ky.anikin@mail.ru>
2019-07-04 07:59:19 +0300

    Add ViewModelKey

---
+++ app/src/main/java/com/anikinkirill/tinkoffsiriusmobile/viewmodel/ViewModelKey.java
@@ -0,0 +1,25 @@
+package com.anikinkirill.tinkoffsiriusmobile.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import dagger.MapKey;
+
+/**
+ * CREATED BY ANIKINKIRILL
+ */
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@MapKey
+public @interface ViewModelKey {
+
+    Class<? extends ViewModel> value();
+
+}
