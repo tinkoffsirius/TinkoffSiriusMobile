eef4de973a13c4bf9d9a6320876f6c74f028c709
ANIKINKIRILL <ky.anikin@mail.ru>
2019-07-04 08:05:08 +0300

    Add SignInViewModel and AuthViewModelModule

---
+++ app/src/main/java/com/anikinkirill/tinkoffsiriusmobile/ui/auth/SignInViewModel.java
@@ -0,0 +1,18 @@
+package com.anikinkirill.tinkoffsiriusmobile.ui.auth;
+
+import androidx.lifecycle.ViewModel;
+
+import javax.inject.Inject;
+
+/**
+ * CREATED BY ANIKINKIRILL
+ */
+
+public class SignInViewModel extends ViewModel {
+
+    @Inject
+    public SignInViewModel(){
+
+    }
+
+}
