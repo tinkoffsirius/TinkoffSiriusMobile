fe1f1f489bb9811712bda3f9168a68ca82bacb16
ANIKINKIRILL <ky.anikin@mail.ru>
2019-07-04 08:13:55 +0300

    Init viewModel in SignInActivity

--- app/src/main/java/com/anikinkirill/tinkoffsiriusmobile/ui/auth/SignInActivity.java
+++ app/src/main/java/com/anikinkirill/tinkoffsiriusmobile/ui/auth/SignInActivity.java
@@ -5,10 +5,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.lifecycle.ViewModelProviders;
+
 import com.anikinkirill.tinkoffsiriusmobile.R;
+import com.anikinkirill.tinkoffsiriusmobile.viewmodel.ViewModelProviderFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
+
+import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
@@ -20,16 +25,32 @@ import dagger.android.support.DaggerAppCompatActivity;
 
 public class SignInActivity extends DaggerAppCompatActivity {
 
+    // Injections
+    @Inject
+    ViewModelProviderFactory providerFactory;
+
     // UI
     private EditText userLogin;
     private EditText userPassword;
+
+    // Vars
+    private SignInViewModel viewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+       init();
+       initViewModel();
+    }
+
+    private void init(){
         userLogin = findViewById(R.id.userLogin);
         userPassword = findViewById(R.id.userPassword);
+    }
+
+    private void initViewModel(){
+        viewModel = ViewModelProviders.of(this, providerFactory).get(SignInViewModel.class);
     }
 
     public void signIn(View v){
