00345141129eaf4ef28a1fc3e88f9d548be4f8e3
ANIKINKIRILL <ky.anikin@mail.ru>
2019-07-05 09:16:25 +0300

    Add request device permissions
    For API > 23 app will request location permissions when opens app

--- app/src/main/java/com/anikinkirill/tinkoffsiriusmobile/ui/auth/AuthActivity.java
+++ app/src/main/java/com/anikinkirill/tinkoffsiriusmobile/ui/auth/AuthActivity.java
@@ -1,18 +1,30 @@
 package com.anikinkirill.tinkoffsiriusmobile.ui.auth;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.anikinkirill.tinkoffsiriusmobile.R;
 import com.anikinkirill.tinkoffsiriusmobile.viewmodel.ViewModelProviderFactory;
+
+import java.security.Permission;
+import java.security.Permissions;
 
 import javax.inject.Inject;
 
@@ -26,6 +38,8 @@ import dagger.android.support.DaggerAppCompatActivity;
 
 public class AuthActivity extends DaggerAppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "AuthActivity";
+
     // Injections
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -38,11 +52,14 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
 
     // Vars
     private AuthViewModel viewModel;
+    private static final int REQUEST_LOCATION_PERMISSIONS_CODE = 1;
+    private String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        checkDevicePermissions();
         init();
         initViewModel();
     }
@@ -76,4 +93,46 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
+    private void checkDevicePermissions(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_DENIED
+            || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestLocationPermissions();
+        }
+    }
+
+    private void requestLocationPermissions(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+            final AlertDialog alertDialog = new AlertDialog.Builder(this)
+                    .setTitle("Locations Permissions")
+                    .setMessage("This permissions are needed for getting your location on the map")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(AuthActivity.this, permissions, REQUEST_LOCATION_PERMISSIONS_CODE);
+                        }
+                    })
+                    .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .create();
+            alertDialog.show();
+        }else {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_LOCATION_PERMISSIONS_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == REQUEST_LOCATION_PERMISSIONS_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                Log.d(TAG, "onRequestPermissionsResult: permissions granted");
+            }else{
+                Log.d(TAG, "onRequestPermissionsResult: permissions denied");
+            }
+        }
+    }
 }
