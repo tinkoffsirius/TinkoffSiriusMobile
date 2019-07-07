0cddf3d369b8bff879102ac994f230e0e0ac3d46
ANIKINKIRILL <ky.anikin@mail.ru>
2019-07-07 11:48:24 +0300

    Add MapViewModel

---
+++ app/src/main/java/com/anikinkirill/tinkoffsiriusmobile/ui/map/MapViewModel.java
@@ -0,0 +1,17 @@
+package com.anikinkirill.tinkoffsiriusmobile.ui.map;
+
+import androidx.lifecycle.ViewModel;
+
+import javax.inject.Inject;
+
+/**
+ * CREATED BY ANIKINKIRILL
+ */
+public class MapViewModel extends ViewModel {
+
+    @Inject
+    public MapViewModel(){
+
+    }
+
+}
