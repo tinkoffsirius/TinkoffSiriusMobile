26b1445b0f9fad88211e828f7252df4342b06f76
aav672 <artemalpatov2@gmail.com>
2019-07-18 18:43:53 +0300

    Showing information if late

--- app/src/main/java/com/anikinkirill/tinkoffsiriusmobile/ui/map/MapActivity.java
+++ app/src/main/java/com/anikinkirill/tinkoffsiriusmobile/ui/map/MapActivity.java
@@ -1,17 +1,20 @@
 package com.anikinkirill.tinkoffsiriusmobile.ui.map;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -47,7 +50,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -70,6 +76,7 @@ public class MapActivity extends DaggerAppCompatActivity implements OnMapReadyCa
 
     // UI
     private GoogleMap googleMap;
+    private TextView textLate;
 
     // Vars
     private FusedLocationProviderClient fusedLocation;
@@ -96,6 +103,12 @@ public class MapActivity extends DaggerAppCompatActivity implements OnMapReadyCa
     }
 
     private void init(){
+        LateChecker lc=new LateChecker();
+        lc.start();
+
+        textLate=(TextView) findViewById(R.id.textLate);
+        textLate.setVisibility(View.GONE);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
@@ -199,5 +212,109 @@ public class MapActivity extends DaggerAppCompatActivity implements OnMapReadyCa
                 googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(getApplicationContext(),R.raw.style));
             }
         }catch(Exception e){}
+    }
+
+    private class LateChecker extends Thread{
+        boolean result=false;
+        String endTime="";
+        @Override
+        public void run() {
+            while(true) {
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+                databaseReference.child(Constants.SOLUTION).child(Constants.AGENTS).addValueEventListener(new ValueEventListener() {
+                    @SuppressLint("MissingPermission")
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Iterable<DataSnapshot> iterable = dataSnapshot.getChildren();
+                        Iterator<DataSnapshot> iterator = iterable.iterator();
+                        while (iterator.hasNext()) {
+                            DataSnapshot next = iterator.next();
+                            if (next.child("agent").child("id").getValue().toString().equals(FirebaseAuth.getInstance().getCurrentUser().getEmail().split("@")[0])) {
+                                Iterable<DataSnapshot> activities = next.child("activities").getChildren();
+                                Iterator<DataSnapshot> activitiesIterator = activities.iterator();
+                                if (activitiesIterator.hasNext()) {
+                                    DataSnapshot a = activitiesIterator.next();
+                                    DataSnapshot activity = a.child("dueTimeSeconds");
+                                    Double lat = Double.parseDouble(a.child("coordinates").child("latitude").getValue() + "");
+                                    Double lon = Double.parseDouble(a.child("coordinates").child("longitude").getValue() + "");
+                                    String time = (Integer.parseInt(activity.getValue().toString() + "")) / 3600 + ":" + (Integer.parseInt(activity.getValue() + "")) % 3600 / 60;
+                                    Log.e("End time", time);
+                                    endTime = time;
+                                    fusedLocation.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Location> task) {
+                                            double lat2 = task.getResult().getLatitude();
+                                            double lon2 = task.getResult().getLongitude();
+                                            double res = calculateDistanse(lat, lat2, lon, lon2);
+                                            double lateTime = res / 6.0 * 60;
+                                            Log.e("Time minutes", lateTime + "");
+                                            Date d = new Date();
+                                            double minutes = d.getMinutes() + lateTime;
+                                            double hours = minutes / 60;
+                                            minutes %= 60;
+                                            String arrivalTime = (int) (d.getHours() + hours) + ":" + (int) (minutes);
+                                            Log.e("Arrival time", arrivalTime + "");
+                                            if (Integer.parseInt(arrivalTime.split(":")[0]) < Integer.parseInt(time.split(":")[0])) {
+                                                runOnUiThread(new Runnable() {
+                                                    @Override
+                                                    public void run() {
+                                                        textLate.setVisibility(View.GONE);
+                                                    }
+                                                });
+                                            } else if (Integer.parseInt(arrivalTime.split(":")[0]) == Integer.parseInt(time.split(":")[0])) {
+                                                if (Integer.parseInt(arrivalTime.split(":")[1]) < Integer.parseInt(time.split(":")[1])) {
+                                                    runOnUiThread(new Runnable() {
+                                                        @Override
+                                                        public void run() {
+                                                            textLate.setVisibility(View.GONE);
+                                                        }
+                                                    });
+                                                } else {
+                                                    runOnUiThread(new Runnable() {
+                                                        @Override
+                                                        public void run() {
+                                                            textLate.setVisibility(View.VISIBLE);
+                                                        }
+                                                    });
+                                                }
+                                            } else {
+                                                runOnUiThread(new Runnable() {
+                                                    @Override
+                                                    public void run() {
+                                                        textLate.setVisibility(View.VISIBLE);
+                                                    }
+                                                });
+                                            }
+                                        }
+                                    });
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+                try {
+                    sleep(2000);
+                }catch(Exception e){
+                    Log.e(TAG,e+"");
+                }
+            }
+        }
+    }
+
+    public double calculateDistanse(double lat1,double lat2,double lon1,double lon2){
+        lat1*=3.1415926/180.00;
+        lat2*=3.1415926/180.00;
+        lon1*=3.1415926/180.00;
+        lon2*=3.1415926/180.00;
+        double dlon=Math.abs(lon2 - lon1);
+        double dlat=Math.abs(lat2 - lat1);
+        double r = 6371;
+        double a = Math.sin(dlat/2)*Math.sin(dlat/2) + Math.cos(lat1)*Math.cos(lat2)*Math.sin(dlon/2)*Math.sin(dlon/2);
+        double c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1-a) );
+        return r*c;
     }
 }
